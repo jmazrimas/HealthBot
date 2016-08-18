8710acd808b475cd4ea0b25635224ab10c9f6b42
jmazrimas <joe.mazrimas@gmail.com>
2016-08-18 17:10:28 -0500

    basic category response

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,8 +7,12 @@ get '/receive-sms' do
 
   body = params["Body"]
 
+  response = classing_bot.classify(body)
+  # see if what was sent initially has an address
+  # if not ask, and keep asking until you get a positive response
+
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "I'm responding to your text that said \"#{body}\""
+    r.Message "Were you looking for info on #{response}?"
   end
   twiml.text
 end
