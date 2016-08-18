5363b1d3e3e022dfd065a64eb34c9fecfa58ecee
jmazrimas <joe.mazrimas@gmail.com>
2016-08-18 17:17:22 -0500

    basic category response

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -8,11 +8,11 @@ get '/receive-sms' do
   body = params["Body"]
   response = classing_bot.classify(body)
 
-  session[:response] = response
-
   msg = "Were you looking for info on #{response}?"
 
   msg += "last = #{session[:response]}" if session[:response]
+
+  session[:response] = response
 
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message msg
