ef617ec3a2ef8c3aa38c8df66954beca4329e4a0
jmazrimas <joe.mazrimas@gmail.com>
2016-08-18 11:47:36 -0500

    using body in response

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,8 +4,10 @@ end
 
 get '/receive-sms' do
 
+  body = params["Body"]
+
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "I'm responding to your text!"
+    r.Message "I'm responding to your text that said \"#{body}\""
   end
   twiml.text
 end
