105a3f5b9fb1f97b6c8ac18c5f153ecf562796fd
Joseph Mazrimas <email": null>
2016-08-19 07:58:06 -0500

    trying to deploy logic

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -51,17 +51,43 @@ get '/receive-sms' do
 
   # exclude = (session[:exclude_categories] ? session[:exclude_categories] : [])
 
-  categories = CategoryManager.new.names_excluding([])
-  classing_bot = create_class_bot(categories)
-  classing_bot.classify(body)
+  # categories = CategoryManager.new.names_excluding([])
+  # classing_bot = create_class_bot(categories)
+  # classing_bot.classify(body)
 
-  body = params["Body"]
-  response = classing_bot.classify(body)
-  msg = "Were you looking for info on #{response}?"
+  # body = params["Body"]
+  # response = classing_bot.classify(body)
+  # msg = "Were you looking for info on #{response}?"
 
+  potential_location = parse_location(from_user)
+
+  if potential_location
+    session[:potential_location] = potential_location
+  end
+
+  if session[:confirmed_category]
+    if session[:potential_location]
+      @res_back = "You're looking for help with #{session[:confirmed_category]} at #{session[:potential_location]}."
+    else
+      @res_back = "Couldn't understand that. What is your address or zip code?"
+    end
+  else
+    if excluded_all_categories?
+      @res_back = "Sorry, I don't think we can help you :("
+    elsif from_user.downcase == "yes"
+      confirm_category
+      session[:last_suggestion] = nil
+      @res_back = "Great! We'll get you some info about #{session[:confirmed_category].gsub("_"," ")}. What is your location?"
+    elsif from_user.downcase == "no"
+      exclude_category
+      @res_back = get_next_suggestion(from_user)
+    else
+      @res_back = get_next_suggestion(from_user)
+    end
+  end
 
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message msg
+    r.Message @res_back
   end
   twiml.text
 end
