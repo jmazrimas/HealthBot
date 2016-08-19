b86328f1cb06728c273fb97ea261f4e21e888c04
Joseph Mazrimas <email": null>
2016-08-19 09:02:56 -0500

    test

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -59,32 +59,32 @@ get '/receive-sms' do
   # response = classing_bot.classify(body)
   # msg = "Were you looking for info on #{response}?"
 
-  # potential_location = parse_location(from_user)
+  potential_location = parse_location(from_user)
 
-  # if potential_location
-  #   session[:potential_location] = potential_location
-  # end
+  if potential_location
+    session[:potential_location] = potential_location
+  end
 
-  # if session[:confirmed_category]
-  #   if session[:potential_location]
-  #     @res_back = "You're looking for help with #{session[:confirmed_category]} at #{session[:potential_location]}."
-  #   else
-  #     @res_back = "Couldn't understand that. What is your address or zip code?"
-  #   end
-  # else
-  #   if excluded_all_categories?
-  #     @res_back = "Sorry, I don't think we can help you :("
-  #   elsif from_user.downcase == "yes"
-  #     confirm_category
-  #     session[:last_suggestion] = nil
-  #     @res_back = "Great! We'll get you some info about #{session[:confirmed_category].gsub("_"," ")}. What is your location?"
-  #   elsif from_user.downcase == "no"
-  #     exclude_category
-  #     @res_back = get_next_suggestion(from_user)
-  #   else
-  #     @res_back = get_next_suggestion(from_user)
-  #   end
-  # end
+  if session[:confirmed_category]
+    if session[:potential_location]
+      @res_back = "You're looking for help with #{session[:confirmed_category]} at #{session[:potential_location]}."
+    else
+      @res_back = "Couldn't understand that. What is your address or zip code?"
+    end
+  else
+    if excluded_all_categories?
+      @res_back = "Sorry, I don't think we can help you :("
+    elsif from_user.downcase == "yes"
+      confirm_category
+      session[:last_suggestion] = nil
+      @res_back = "Great! We'll get you some info about #{session[:confirmed_category].gsub("_"," ")}. What is your location?"
+    elsif from_user.downcase == "no"
+      exclude_category
+      @res_back = get_next_suggestion(from_user)
+    else
+      @res_back = get_next_suggestion(from_user)
+    end
+  end
 
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message "hello to you!"
